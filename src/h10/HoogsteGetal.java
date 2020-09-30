94f8444835ce80804fe214b92c890ee03cdd4c88
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-09-30 16:25:38 +0200

    h10.1af

--- src/h10/HoogsteGetal.java
+++ src/h10/HoogsteGetal.java
@@ -8,62 +8,67 @@ import java.awt.event.ActionListener;
 
 public class HoogsteGetal extends Applet {
     //general
-    Color mint;
-    String hoogstecijfer,getal;
-    //teskstvak
-    TextField texvak;
-    //knop
-    Button push;
-    //label
+    Color blauw;
+    //textfield
+    TextField texv1;
+    String print;
+
+
 
 
     @Override
     public void init() {
         //general
-        mint = new Color(189, 248, 215, 206);
-
-        setBackground(mint);
-        hoogstecijfer = ("De hoogste cijfer is:");
-        //knop
-        push = new Button();
-        add(push);
-        push.addActionListener(new Pushknop());
-        //tekstvak
-        texvak = new TextField();
-        add(texvak);
+        blauw = new Color(174, 206, 243);
+        setBackground(blauw);
+        //textfield
+        texv1 = new TextField("");
+        add(texv1);
+        texv1.addActionListener(new Al());
+        print = "...";
     }
+
+
 
     @Override
     public void paint(Graphics g) {
         //general
-        g.drawString(hoogstecijfer,30,50);
-        g.drawString(String.valueOf(getal),40,60);
-        //knop
-        push.setSize(35,50);
-        push.setLabel("Enter");
-        push.setLocation(200,50);
-        //tekstvak
-        texvak.setSize(100,50);
-        texvak.setLocation(50,50);
+        g.drawString("Welke getal is de grootste getal?",30,50);
+        g.drawString("10, 5, 20, 43",30,65);
+        //textfield
+        texv1.setLocation(30,70);
+        texv1.setSize(50,25);
 
+        g.drawString(print,30,110);
     }
 
-    class Pushknop implements ActionListener{
+
+
+    public class Al implements ActionListener{
 
         @Override
         public void actionPerformed(ActionEvent e) {
             //general
-            texvak.getText();
+            System.out.println("test");
+            String user_input = texv1.getText();
 
+            //if (user_input.equals("9")) { print = "Helaas dit is niet de goede."; }
+            //else { print = "Deze getal staat niet in dit rijtje...";}
+            //if (user_input.equals("5")) { print = "Helaas dit is niet de goede.";}
+            //else { print = "Deze getal staat niet in dit rijtje...";}
+            //repaint();
 
+            switch (user_input) {
+                case "10": print = "Helaas dit is niet de goede"; break;
+                case "5": print = "Helaas dit is niet de goede"; break;
+                case "20": print = "Helaas dit is niet de goede"; break;
+                case "43": print = "43, Yess dit is de grootste getaal in het rijtje!"; break;
+                default: print =  "Dit cijfer staat niet in het rijtje"; break;
+            }
+            texv1.setText("");
+            repaint();
 
-
-
-
-            //tot hier
-            int getal;
-            getal = Integer.parseInt(texvak.getText());
-
+            //if (user_input > 0){ print = "Yess dit is de grootste getal.";}
         }
     }
 }
