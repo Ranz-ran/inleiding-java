2cb2c05071e0763df4f0293d8ca8072eb18e7a02
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-10-07 18:28:19 +0200

    bezig H11_1

---
+++ src/h11/H11_1.java
@@ -0,0 +1,20 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H11_1 extends Applet {
+
+
+    @Override
+    public void init() {
+
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        
+    }
+}
