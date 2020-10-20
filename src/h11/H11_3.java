e778ccfc63ec1fc44c7a3d439f579fb0d09b88bf
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-10-20 15:58:45 +0200

    bezig h11_3

---
+++ src/h11/H11_3.java
@@ -0,0 +1,26 @@
+package h11;
+//Geef de rij van Fibonacci. De eerste twee getallen van de rij zijn 1 en de volgende zijn de som van de twee voorgangers.
+//1, 1, 2, 3, 5, 8, 13, 21, en zo voort...
+import java.applet.Applet;
+import java.awt.*;
+
+public class H11_3 extends Applet {
+    //general
+    int eerstGetal;
+    Color paars;
+
+    @Override
+    public void init() {
+        paars = new Color(204, 176, 239);
+        setBackground(paars);
+
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+
+    }
+
+}
