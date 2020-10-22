7731ca9cad419034d451ca885577c2c7afe03087
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-10-22 13:37:35 +0200

    h11.3 af

--- src/h11/H11_3.java
+++ src/h11/H11_3.java
@@ -6,13 +6,23 @@ import java.awt.*;
 
 public class H11_3 extends Applet {
     //general
-    int eerstGetal;
+    int eerstGetal,tweedeGetal,som,y;
     Color paars;
+
 
     @Override
     public void init() {
+        //general
         paars = new Color(204, 176, 239);
         setBackground(paars);
+
+        eerstGetal = 1;
+        tweedeGetal = 1;
+        som = 0;
+        y = 0;
+        //inhoud
+
+
 
 
     }
@@ -21,6 +31,28 @@ public class H11_3 extends Applet {
     public void paint(Graphics g) {
 
 
+
+        for(int teller =1 ; teller<= 10 ; teller++ ){
+
+            y += 20;
+
+            som = eerstGetal + tweedeGetal;
+            g.drawString(""+som,200,y);
+            eerstGetal = tweedeGetal;
+            tweedeGetal= som;
+
+            //n3=n1+n2;
+            //  System.out.print(" "+n3);
+            //  n1=n2;
+            //  n2=n3;
+
+        }
+
+
+
+
     }
+
+
 
 }
