27734223c61bfbdacc332e0940311cd15bf429ef
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-11-04 16:04:21 +0100

    H1201.java is Af & bezig met H1202.java

--- src/h12/H1202.java
+++ src/h12/H1202.java
@@ -1,6 +1,25 @@
 package h12;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class H1202 extends Applet {
+
+    Button[] b;
+
+    @Override
+    public void init() {
+        b = new Button[25];
+        add(b[25]);
+
+        for (int i = 0; i < b.length; i++ );
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        for (int teller = 0; teller < b.length; teller++);
+        b[teller] = 100 * teller + 100;
+    }
+
 }
