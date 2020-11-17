ad27946d70d6bd7118b2b4e1c1868fb422e21174
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-11-17 16:18:04 +0100

    bezig H1205.java

--- src/h12/H1205.java
+++ src/h12/H1205.java
@@ -3,11 +3,28 @@ package h12;
 // gevonden is. Zoek op internet hoe je dit kunt doen.
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class H1205 extends Applet {
 
+    boolean found;
+    int[] tabel = {1,3,4,8,11,14,15};
+    int y,max;
+    TextField txv;
+    Button ok;
+
+
     @Override
     public void init() {
+
+        y = 70;
+
+        txv = new TextField();
+        add(txv);
+        ok = new Button("ok");
+        add(ok);
+        ok.addActionListener(new Al());
 
 
     }
@@ -15,6 +32,33 @@ public class H1205 extends Applet {
     @Override
     public void paint(Graphics g) {
 
+        // g.drawString("De waarde is gevonden:) " + max, 50, 50);
+        
+
+        if (found == true){
+            g.drawString("waarde is gevonden : )    " + "En index is: " + max,20,50);
+        }
+        else {g.drawString("waarde is niet gevonden...",20,50);}
 
     }
+
+    class Al implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            found = false;
+            int gegevenGetal = Integer.parseInt(txv.getText());
+            for (int i = 0; i < tabel.length; i++){
+                if (tabel[i] == gegevenGetal){
+                    found = true;
+                    max = i;
+                }
+
+            }
+            repaint();
+        }
+    }
+
+
 }
