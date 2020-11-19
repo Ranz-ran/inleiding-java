12256549a527e07ea5831c8e9579b4735ce8dad9
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-11-19 22:57:06 +0100

    Alles zijn af

---
+++ src/h13/H1301.java
@@ -0,0 +1,31 @@
+package h13;
+/*Maak een applet in Java met een methode om een driehoek te tekenen. De declaratie van de methode ziet er als volgt uit:
+
+        void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+        // teken een driehoek m.b.v. de meegegeven data (parameters)
+        }
+De methode roep je aan vanuit paint(). De argumenten x1, y1 en x2, y2 en x3, y3 zijn de coördinaten van de
+hoekpunten van de driehoek. De methode vraagt om een object van het type Graphics. Deze kun je doorgeven vanuit
+de methode paint().
+ */
+import java.applet.Applet;
+import java.awt.*;
+
+public class H1301 extends Applet {
+
+    @Override
+    public void init() {
+
+
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+
+
+    }
+
+
+}
