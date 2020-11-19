80ea8fad9e9aa712271ea0c73998ccfe1110d765
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-11-19 13:56:20 +0100

    Alles zijn af

--- src/h12/P12.java
+++ src/h12/P12.java
@@ -6,35 +6,67 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.Array;
 
 public class P12 extends Applet {
 
-    int[] tabel = new int[i];
+    int[] nummer = new int[10];
+    String[] naam = new String[10];
+    int y,i;
     Button ok;
-    TextField txv;
+    TextField txv,txv2;
+    boolean volleArray;
 
     @Override
     public void init() {
+
+        i=0;
 
         ok = new Button("ok");
         add(ok);
         ok.addActionListener(new Al());
         txv = new TextField();
+        add(txv);
+        txv2 = new TextField();
+        add(txv2);
 
-
+        y=60;
     }
 
     @Override
     public void paint(Graphics g) {
 
+        ok.setLocation(90, 30);
 
+        txv.setLocation(30, 30);
+        txv.setSize(60, 25);
+        txv2.setLocation(200, 30);
+        txv2.setSize(60, 25);
+
+        if (volleArray) {
+            for (i = 0; i < 10; i++) {
+
+
+                g.drawString("" + naam[i] + "" + nummer[i], 50, 20 * i + 20);
+
+            }
+        }
     }
 
     class Al implements ActionListener{
 
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            if(i >=10) {
+                volleArray = true;
+            }else {
+
+                String s = txv.getText();
+                String t = txv2.getText();
+                naam[i] = s;
+                nummer[i] = Integer.parseInt(t);
+                i++;
+            }repaint();
 
         }
     }
