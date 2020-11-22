9ed18790c77089869f64166839f3a4672f9a1aa7
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-11-22 22:35:55 +0100

    H13.1 af

--- src/h13/H1301.java
+++ src/h13/H1301.java
@@ -13,6 +13,7 @@ import java.awt.*;
 
 public class H1301 extends Applet {
 
+
     @Override
     public void init() {
 
@@ -22,9 +23,16 @@ public class H1301 extends Applet {
 
     @Override
     public void paint(Graphics g) {
+        tekenDriehoek(g,30,130,130,130,80,30);
 
 
+    }
 
+    public void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+        //lijne van diehoek
+        g.drawLine(x1,y1,x2,y2);
+        g.drawLine(x2,y2,x3,y3);
+        g.drawLine(x3,y3,x1,y1);
     }
 
 
