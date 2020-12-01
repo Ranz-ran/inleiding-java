644bb1b23e403dee83efabc78961efa4e646264f
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-12-01 08:56:48 +0100

    bezig H1303.java & P13.java

--- src/h13/H1303.java
+++ src/h13/H1303.java
@@ -3,9 +3,104 @@ package h13;
 // hij een muur met rode bakstenen te zien en als hij op de andere knop klikt, krijgt hij een muur met grijze
 // betonblokken te zien. De betonblokken zijn groter dan de bakstenen. Bedenk zelf welke methodes je nodig hebt
 // met welke argumenten.
+
 import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class H1303 extends Applet {
 
+    Color rood,grijs,muurkleur;
+    Button red,gray;
+    boolean click,click2;
+
+    @Override
+    public void init() {
+
+        setSize(600,300);
+
+        rood = new Color(165, 30, 30);
+        grijs = new Color(132, 124, 124);
+
+        red = new Button("red");
+        add(red);
+        //red.addActionListener(new Al());
+
+        gray = new Button("gray");
+        add(gray);
+        red.addActionListener(new Al1());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        red.setLocation(100,270);
+        gray.setLocation(190,270);
+
+        if(click = true){
+            muur(g,rood, 10, 10,50, 25);
+
+        }else if (click2 = true){
+            muurGroot(g, grijs, 10, 10, 50, 25);
+        }
+
+
+    }
+
+    public void muur (Graphics g,Color muurkleur,int kolom,int rij, int w, int h){
+        g.setColor(muurkleur);
+        for(int i = 0; i<rij; i++) {
+            for (int n = 0; n<kolom; n++) {
+                if (i % 2 == 0){
+                    g.fillRect((n*w)+n + (int)((double)w*0.5), i*h+i, w, h);
+                }else{
+                    g.fillRect(n*w+n, i*h+i, w, h);
+                }
+            }
+        }
+    }
+
+    public void muurGroot (Graphics g,Color muurkleur,int kolom,int rij, int w, int h){
+        g.setColor(muurkleur);
+        for(int i = 0; i<rij; i++) {
+            for (int n = 0; n<kolom; n++) {
+                if (i % 2 == 0){
+                    g.fillRect(((n*w)+n + (int)((double)w*0.5))*2, (i*h+i)*2, w*2, h*2);
+                }else{
+                    g.fillRect((n*w+n)*2, (i*h+i)*2, w*2, h*2);
+                }
+            }
+        }
+    }
+
+
+
+    class Al implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+            getGraphics().setColor(rood);
+            System.out.println("test1");
+
+            click = true;
+            click2 = false;
+            repaint();
+        }
+    }
+
+    class Al1 implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            getGraphics().setColor(muurkleur = grijs);
+            System.out.println("test2");
+
+            click = false;
+            click2 = true;
+            repaint();
+        }
+    }
+
+
+
 
 }
+
--- src/h13/P13.java
+++ src/h13/P13.java
@@ -1,12 +1,25 @@
 package h13;
 //1. Maak een applet met een methode om een eenvoudige boom te tekenen. Bedenk zelf welke argumenten de methode
 // zou moeten hebben.
-//
 //2. Gebruik de methode uit opdracht 1 om een boomgaard te tekenen die uit tenminste 2 rijen van minimaal 5 bomen
 // bestaat.
-//
-//Als je klaar bent, lever je de broncode in
+
 import java.applet.Applet;
+import java.awt.*;
 
 public class P13 extends Applet {
+
+    @Override
+    public void init() {
+
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+       // boom(g,green,stam)
+
+    }
+
+    //public void boom{g,Color green, }
 }
