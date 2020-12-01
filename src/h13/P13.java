016a059f0f5d390d85dcb6f153176c96194330ea
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-12-01 15:48:02 +0100

    bezig H1303 & P13

--- src/h13/P13.java
+++ src/h13/P13.java
@@ -3,23 +3,48 @@ package h13;
 // zou moeten hebben.
 //2. Gebruik de methode uit opdracht 1 om een boomgaard te tekenen die uit tenminste 2 rijen van minimaal 5 bomen
 // bestaat.
-
 import java.applet.Applet;
 import java.awt.*;
 
 public class P13 extends Applet {
+    int xRect,yRect,wRect,hRect,xOval,yOval,wOval,hOval;
+    Color boomStam,bladeren;
 
-    @Override
     public void init() {
-
-
+        //beeld
+        setSize(500,350);
+        //boom stam
+        xRect = 25;
+        yRect = 70;
+        wRect = 20;
+        hRect = 100;
+        boomStam = new Color(147, 82, 48);
+        //bladeren
+        xOval = 5;
+        yOval = 50;
+        wOval = 70;
+        hOval = 100;
+        bladeren = new Color(161, 219, 133);
     }
-
-    @Override
     public void paint(Graphics g) {
-       // boom(g,green,stam)
-
+        Boom(g);
     }
-
-    //public void boom{g,Color green, }
+    public void Boom(Graphics g){
+        for (int i = 0; i < 2; i++) {
+            for (int n = 0; n < 5; n++) {
+                //boom stam
+                g.setColor(boomStam);
+                g.fillRect(xRect, yRect, wRect, hRect);
+                xRect += wRect + 70;
+                //bladeren
+                g.setColor(bladeren);
+                g.fillOval(xOval, yOval, wOval, hOval);
+                xOval += wOval + 20;
+            }
+            yRect += 150;
+            xRect = 20;
+            yOval += 150;
+            xOval = 0;
+        }
+    }
 }
