715d34074df99b8b0f87589e8e995c7af38352c2
Ranz <62795553+Ranz-ran@users.noreply.github.com>
2020-12-02 11:53:55 +0100

    bezig H1401.java

---
+++ src/h14/H1401.java
@@ -0,0 +1,23 @@
+package h14;
+//Schrijf een applet waarin het delen van één kaart wordt gesimuleerd. Er wordt willekeurig een kleur en
+// binnen die kleur een willekeurige kaart gekozen. Maak een array voor de kleuren en één voor de kaarten
+// binnen de kleuren. Laat de keuze als volgt in het venster van de applet zien, bijvoorbeeld: Ruiten aas.
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H1401 extends Applet {
+
+    @Override
+    public void init() {
+
+
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+
+    }
+}
